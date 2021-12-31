3e504f663edcfe0d1f1c56fa024c14e36811f868
slytechadmin <slytechrootadmin@protonmail.com>
2021-12-30 23:34:12 -0800

    First AK commit

---
+++ Week1/Lab1Ex3.py
@@ -0,0 +1,14 @@
+#Objective: Create a program to display your name, age, major using variables
+
+
+#declare myname variable
+myname = "Attila"
+print ('My name is',myname)
+
+#declare myage variable
+myage = 41
+print ('And I am',myage)
+
+#declare mymajor
+mymajor = "Business Admin and IS"
+print ('I majored Dec. 2021 in',mymajor)
---
+++ Week1/Lab1Ex4.py
@@ -0,0 +1,23 @@
+# Create a program that asks the user to enter name, age, height and weight
+# using input() function. Then use display() function to display the information
+# entered by user.
+
+#prompt for name
+user_name = input ('Enter your name: ')
+print("Hello " + user_name + "!")
+
+#prompt for age
+user_age = input ('Enter your age: ')
+print(user_age)
+
+#prompt for height
+user_height = input ('Enter your height: ')
+print(user_height)
+
+#prompt for height
+user_weight = input ('Enter your weight: ')
+print(user_weight)
+
+#narrative with print() function
+print("So your name is ", user_name, ", and you're", user_age, ",with a height of",
+      user_height, ",and your weight is", user_weight)
